c37d5130248a097e79e88838cb4bc3f5f45e09f4
Samuel-rbd <0001182518@SENAIMGALUNO.COM.BR>
2025-09-01 19:47:28 -0300

    Update Program.cs

--- GHVS/Program.cs
+++ GHVS/Program.cs
@@ -1,38 +1,45 @@
 ﻿using System;
-
 namespace CalculadoraWeb
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculadora Colaborativa Web");
-            // Integrante 1: Implementar menu e chamar as funções
-        }
+            Console.WriteLine("Calculadora Colaborativa ");
+                   }
 
         // Integrante 1
-        public static double Adicao(double a, double b)
-        {
-            return a + b;
-        }
-
+class CalculadoraSoma
+{
+    static void Main()
+    {
+         int operação = int.Parse (Console.ReadLine());
+            Console.WriteLine ("digite o primeiro numero:");
+                double num1 = double.Parse (Console.ReadLine());
+            Console.WriteLine ("digite o segundo numero:");
+                 double num2 = double.Parse (Console.ReadLine());
+                double resultado = 0;
+                   if (operação==1)
+                    {
+                        resultado = num1+num2;
+                    }       
+    }
         // Integrante 2
-        // public static double Subtracao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+       Console.WriteLine("Subtração");
+      
 
         // Integrante 3
-        // public static double Multiplicacao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+       Console.WriteLine("Multiplicação");
 
+        
         // Integrante 4
-        // public static double Divisao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+        Console.WriteLine("Divisão");
+
+       // Integrante 5
+        Console.WriteLine("potenciação");
+    }
+}
+
 
 
   // Integrante 5
@@ -42,3 +49,4 @@ namespace CalculadoraWeb
         // }
     }
 }
+
